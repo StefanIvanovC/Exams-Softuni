33bcf83ea50b60774da8a35794ec635c238915a0
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-03-27 20:19:10 +0200

    Add Exam Preparation EntityFrameworkCore 50 Database creation and add DB sets add two DTO objects

---
+++ Entity Framework Core - 14 August 2020/SoftJail/DataProcessor/Deserializer.cs
@@ -0,0 +1,87 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.Data.Models;
+    using SoftJail.DataProcessor.ImportDto;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+
+    public class Deserializer
+    {
+        public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
+        {
+            var sb = new StringBuilder();
+
+            var departments = new List<Department>();
+
+            var departmentCells = JsonConvert.DeserializeObject<IEnumerable<DepartmentCellInputModel>>(jsonString);
+
+            foreach (var departmentCell in departmentCells)
+            {
+                if(!IsValid(departmentCell) || !departmentCell.Cells.All(IsValid) || !departmentCell.Cells.Any())
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                };
+
+                var department = new Department
+                {
+                    Name = departmentCell.Name,
+                    Cells = departmentCell.Cells.Select(x => new Cell
+                    {
+                        CellNumber = x.CellNumber,
+                        HasWindow = x.HasWindow,
+
+                    })
+                    .ToList()
+                };
+
+                departments.Add(department);
+                sb.AppendLine($"Imported {department.Name} with {department.Cells.Count} cells");
+                
+            }
+            context.Departments.AddRange(departments);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+
+        }
+
+        public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
+        {
+            var sb = new StringBuilder();
+
+            var prisonerMails = JsonConvert
+                .DeserializeObject<ImportPrisonersMailsInputModel>(jsonString);
+
+            foreach (var prisoner in prisonerMails)
+            {
+                if (!IsValid(prisoner) || prisoner.mails)
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+            }
+            
+        }
+
+        public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+    }
+}
---
+++ Entity Framework Core - 14 August 2020/SoftJail/DataProcessor/ImportDto/ImportPrisonersMailsInputModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class ImportPrisonersMailsInputModel
+    {
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Nickname { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime IncarcerationDate { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal? Bail { get; set; }
+
+        public int CellId { get; set; }
+
+        public IEnumerable<MailsInputModel> Mails { get; set; }
+    }
+    public class MailsInputModel
+    {
+        public string Description { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
+
+//Id – integer, Primary Key
+//FullName – text with min length 3 and max length 20 (required)
+//Nickname – text starting with "The " and a single word only of letters with an uppercase letter for beginning(example: The Prisoner) (required)
+//Age – integer in the range[18, 65] (required)
+//IncarcerationDate ¬– Date(required)
+//ReleaseDate– Date
+//Bail– decimal(non - negative, minimum value: 0)
+//CellId - integer, foreign key
+//Cell – the prisoner's cell
+//Mails - collection of type Mail
+//PrisonerOfficers - collection of type OfficerPrisoner
+
+
+//[
+//  {
+//    "FullName": "",
+//    "Nickname": "The Wallaby",
+//    "Age": 32,
+//    "IncarcerationDate": "29/03/1957",
+//    "ReleaseDate": "27/03/2006",
+//    "Bail": null,
+//    "CellId": 5,
+//    "Mails": [
+//      {
+//        "Description": "Invalid FullName",
+//        "Sender": "Invalid Sender",
+//        "Address": "No Address"
+//      },
+//      {
+//    "Description": "Do not put this in your code",
+//        "Sender": "My Ansell",
+//        "Address": "ha-ha-ha"
+//      }
+//    ]
+//  },
