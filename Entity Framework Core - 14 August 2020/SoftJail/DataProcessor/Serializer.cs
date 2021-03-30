8b2a0edc24b7e5e1cebb04f3ecdfc95a76220d9b
Stefan <58903216+StefanIvanovC@users.noreply.github.com>
2021-03-30 22:04:37 +0300

    Add 4th method for Export and serialization

--- Entity Framework Core - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
+++ Entity Framework Core - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
@@ -2,16 +2,55 @@
 {
 
     using Data;
+    using Newtonsoft.Json;
     using System;
+    using System.Linq;
 
     public class Serializer
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            throw new NotImplementedException();
-        }
+            var prisoners = context.Prisoners
+                .Where(p => ids.Contains(p.Id))
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers.Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name
+                    })
+                    .OrderBy(o => o.OfficerName)
+                    .ToList(),
+                    TotalOfficerSalary = decimal.Parse(p.PrisonerOfficers
+                        .Sum(p => p.Officer.Salary)
+                        .ToString("F2"))
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToList();
 
-        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+            var result = JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+
+            return result;
+        }
+    //"Id": 3,
+    //"Name": "Binni Cornhill",
+    //"CellNumber": 503,
+    //"Officers": [
+    //  {
+    //    "OfficerName": "Hailee Kennon",
+    //    "Department": "ArtificialIntelligence"
+    //  },
+    //  {
+    //    "OfficerName": "Theo Carde",
+    //    "Department": "Blockchain"
+    //  }
+
+
+public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
         {
             throw new NotImplementedException();
         }
